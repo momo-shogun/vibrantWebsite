338e1f9f51ccd891a13e9047d2fb55d23c519bee
momo_shogun <krishnafinger12345@gmail.com>
2025-03-03 16:57:49 +0530

    updated header

--- src/js/app.js
+++ src/js/app.js
@@ -37,10 +37,10 @@ function toggleMenu() {
 }
 
 //gradient
-const b1 = "linear-gradient(240deg, #f86f3d59, rgba(232, 121, 249, 0.26) 40.92%, rgba(41, 104, 240, 0.123) 70.35%)";
+// const b1 = "linear-gradient(240deg, #f86f3d59, rgba(232, 121, 249, 0.26) 40.92%, rgba(41, 104, 240, 0.123) 70.35%)";
 
-document.querySelector("#a").style.background = b1;
-document.querySelector("#a").style.backgroundSize = "300% 300%"; // Larger background area for smooth movement
+// document.querySelector("#a").style.background = b1;
+// document.querySelector("#a").style.backgroundSize = "300% 300%"; // Larger background area for smooth movement
 
 gsap.to("#a", {
     backgroundPosition: "40% 23%", // Moves the gradient
