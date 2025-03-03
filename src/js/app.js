601de20fee57049200f10480fbcd3199d2ca3912
momo_shogun <krishnafinger12345@gmail.com>
2025-03-03 09:45:38 +0530

    init

---
+++ src/js/app.js
@@ -0,0 +1,61 @@
+// main.js
+import {
+    initHeroAnimation,
+    initStatsAnimation,
+    initFeaturesAnimation,
+    initProcessAnimation,
+    initTestimonialsAnimation
+} from './gsapAnimation.js';
+
+// slick slider
+$(document).ready(function () {
+    // Initialize Owl Carousel with optimal settings
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        dots: false,
+        autoplayHoverPause: true,
+        autoplaySpeed: 8000,
+        slideTransition: 'linear',
+        draggable: true,
+        fade: true,
+        pauseOnFocus: true,
+        infinite: true,
+        responsive: {
+            0: { items: 1 },
+            600: { items: 2 },
+            1000: { items: 3 }
+        }
+    });
+});
+
+//menu
+function toggleMenu() {
+    var menu = document.getElementById("mobile-menu");
+    menu.classList.toggle("hidden");
+}
+
+//gradient
+const b1 = "linear-gradient(240deg, #f86f3d59, rgba(232, 121, 249, 0.26) 40.92%, rgba(41, 104, 240, 0.123) 70.35%)";
+
+document.querySelector("#a").style.background = b1;
+document.querySelector("#a").style.backgroundSize = "300% 300%"; // Larger background area for smooth movement
+
+gsap.to("#a", {
+    backgroundPosition: "40% 23%", // Moves the gradient
+    duration: 100,
+    ease: "linear",
+    repeat: -1,
+    yoyo: true
+});
+
+//gsap animation
+
+document.addEventListener("DOMContentLoaded", () => {
+    initHeroAnimation();
+    initStatsAnimation();
+    initFeaturesAnimation();
+    initProcessAnimation();
+    initTestimonialsAnimation();
+});
---
+++ src/js/locomotiveScroll.js
@@ -0,0 +1,30 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.scroll-container'),
+    smooth: true,
+    smartphone: { smooth: true },
+    tablet: { smooth: true },
+});
+
+// Update ScrollTrigger when Locomotive Scroll updates
+scroll.on("scroll", ScrollTrigger.update);
+
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.scrollerProxy(".scroll-container", {
+    scrollTop(value) {
+        return arguments.length ? scroll.scrollTo(value, 0, 0) : scroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+        return {
+            top: 0,
+            left: 0,
+            width: window.innerWidth,
+            height: window.innerHeight
+        };
+    },
+    // pinType: document.querySelector(".scroll-container").style.transform ? "transform" : "fixed"
+});
+
+// Refresh ScrollTrigger after Locomotive is fully loaded
+ScrollTrigger.addEventListener("refresh", () => scroll.update());
+ScrollTrigger.refresh();
